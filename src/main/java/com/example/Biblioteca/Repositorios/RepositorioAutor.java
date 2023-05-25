b893261e25229e100b73d7ba59b14a64e5552847
Lfgomezj <123104490+Lfgomezj@users.noreply.github.com>
2023-05-25 14:46:05 -0500

    se crea identidad Autor y su repositorio

--- src/main/java/com/example/Biblioteca/Repositorios/RepositorioAutor.java
+++ src/main/java/com/example/Biblioteca/Repositorios/RepositorioAutor.java
@@ -1,10 +1,9 @@
 package com.example.Biblioteca.Repositorios;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.Biblioteca.Entidades.Editorial;
+import com.example.Biblioteca.Entidades.Autor;
 
 @Repository
-public interface RepositorioEditorial extends JpaRepository<Editorial, Integer>{
+public interface RepositorioAutor extends JpaRepository<Autor,Integer> {
 }
